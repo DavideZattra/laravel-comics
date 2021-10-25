c71c0072f8471cb7bf7792771c996c044d77e9aa
DavideZattra <d.zattra@gmail.com>
2021-10-25 15:15:09 +0200

    created scaffonlding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<ul>
+    <li><a href="{{ route('characters') }}"></a></li>
+    <li><a href="{{ route('comics') }}"></a></li>
+    <li><a href="{{ route('movies') }}"></a></li>
+    <li><a href="{{ route('tv') }}"></a></li>
+    <li><a href="{{ route('games') }}"></a></li>
+    <li><a href="{{ route('collectibles') }}"></a></li>
+    <li><a href="{{ route('videos') }}"></a></li>
+    <li><a href="{{ route('fans') }}"></a></li>
+    <li><a href="{{ route('news') }}"></a></li>
+    <li><a href="{{ route('shop') }}"></a></li>
+</ul>
