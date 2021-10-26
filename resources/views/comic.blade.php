cbb7d972fb1b112c136db64cec1c3de2b33748d2
DavideZattra <d.zattra@gmail.com>
2021-10-26 16:13:20 +0200

    working on content

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -14,24 +14,68 @@
             </figure>
         </div>
         <div class="main-content">
-            <h2>{{ $comic["title"] }}</h2>
-            <p>{{ $comic["description"] }}</p>
-            <img src="{{ asset('images/adv.jpg') }}" alt="">
-            <p>
-                
-                @foreach ($comic["artists"] as $artist)
-
-                    <a href="#">{{ $artist }}</a>
+            <div class="comic-description">
+                <div class="left-side">
                     
-                @endforeach
-            </p>
-            <p>
-                @foreach ($comic["writers"] as $writer)
+                    <h2>{{ $comic["title"] }}</h2>
+                    <div class="availability">
+                        <div class="status">
+                            <p>Us price {{ $comic["price"] }}</p>
+                            <p>Available</p>
+                        </div>
+                        <div class="check">
+                            <p>Check Availability &downarrow;</p>
+                        </div>
+                    </div>
+                    <p>{{ $comic["description"] }}</p>
 
-                    <a href="#">{{ $writer }}</a>
+                </div>
+                <div class="right-side">
                     
-                @endforeach
-            </p>
+                    <img src="{{ asset('images/adv.jpg') }}" alt="">
+
+                </div>
+            </div>
+
+            <div class="comic-spec">
+                <div class="left-side">
+                    <h2>Talent</h2>
+                    <div class="artist">
+                        <p>Art by:</p>
+                        <p>
+                            @foreach ($comic["artists"] as $artist)
+
+                                <a href="#">{{ $artist }}</a>,
+                    
+                            @endforeach
+                        </p>
+                    </div>
+                    <div class="writers">
+                        <p>written by:</p>
+                        <p>
+                            @foreach ($comic["writers"] as $writer)
+
+                                <a href="#">{{ $writer }}</a>,
+                    
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+                <div class="right-side">
+                    <h2>Specs</h2>
+                    <div class="series">
+                        
+                    </div>
+                    <div class="price">
+
+                    </div>
+                    <div class="sale">
+
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
         @dump($comic)
     </div>
